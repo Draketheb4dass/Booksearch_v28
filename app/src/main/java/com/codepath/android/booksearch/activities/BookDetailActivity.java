47b352c9e8d90f7a4441a1243f39dfd0a9e92820
Jephte Drake Colin <drakecolinj@gmail.com>
2019-04-16 15:23:52 -0400

    Add share Intent WIP
    Signed-off-by: Jephte Drake Colin <drakecolinj@gmail.com>

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -1,25 +1,49 @@
 package com.codepath.android.booksearch.activities;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.Nullable;
+import android.support.v4.content.FileProvider;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.codepath.android.booksearch.GlideApp;
 import com.codepath.android.booksearch.R;
 import com.codepath.android.booksearch.models.Book;
+
+import java.io.File;
 
 public class BookDetailActivity extends AppCompatActivity {
     private ImageView ivBookCover;
     private TextView tvTitle;
     private TextView tvAuthor;
+    private ShareActionProvider miShareAction;
+    private Intent shareIntent;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         // Fetch views
         ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
         tvTitle = (TextView) findViewById(R.id.tvTitle);
@@ -31,14 +55,56 @@ public class BookDetailActivity extends AppCompatActivity {
         // Use book object to populate data into views
         tvTitle.setText(book.getTitle());
         tvAuthor.setText(book.getAuthor());
-        GlideApp.with(this).load(book.getCoverUrl()).into(ivBookCover);
+        GlideApp.with(this).load(book.getCoverUrl())
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+
+                        prepareShareIntent(((BitmapDrawable) resource).getBitmap());
+                        attachShareIntentAction();
+                        return false;
+                    }
+                }).into(ivBookCover);
+
     }
+
+    // Gets the image URI and setup the associated share intent to hook into the provider
+    public void prepareShareIntent(Bitmap drawableImage) {
+        // Fetch Bitmap Uri locally
+        // getExternalFilesDir() + "/Pictures" should match the declaration in fileprovider.xml paths
+        File file = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "share_image_" + System.currentTimeMillis() + ".png");
+
+        // wrap File object into a content provider. NOTE: authority here should match authority in manifest declaration
+        Uri bmpUri = FileProvider.getUriForFile(this, "com.codepath.fileprovider", file);
+        //Uri bmpUri = getBitmapFromDrawable(drawableImage);// see previous remote images section and notes for API > 23
+        // Construct share intent as described above based on bitmap
+        shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, bmpUri);
+        shareIntent.setType("image/*");
+    }
+
+    // Attaches the share intent to the share menu item provider
+    public void attachShareIntentAction() {
+        if (miShareAction != null && shareIntent != null)
+            miShareAction.setShareIntent(shareIntent);
+    }
+
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+        MenuItem item = menu.findItem(R.id.menu_item_share);
+        // Fetch reference to the share action provider
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+
         return true;
     }
 
